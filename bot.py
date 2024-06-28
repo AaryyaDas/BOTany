78d17e3cea71ca74a8481947ff209240670df7ed
Aaryya Das <dasaaryya002@gmail.com>
2024-06-28 22:41:56 +0530

    Added 2 slash commands to bot.py, viz., Translate, & Reminder

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from googletrans import Translator
+import random
+
+#Initializing the bot
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+#Intializing the Google ranslate API
+translator = Translator()
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+#Since I want to use slash commands, I tried to not add a command prefix
+#But couldn't find a reliable way to do that in the brief search I did
+
+#Defining what to do on events
+@bot.event
+async def on_ready():
+    print(f'Successfully logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} command(s)')
+    except Exception as e:
+        print(e)
+
+#Defining slash commands
+#Translate command
+@bot.tree.command(name="translate", description="Translate text to a specified language")
+async def translate(interaction: discord.Interaction, text: str, dest_lang: str):
+    translation = translator.translate(text, dest=dest_lang)
+    await interaction.response.send_message(f'Translated to {dest_lang}: {translation.text}')
+
+#Reminder/Alarm command
+@bot.tree.command(name="reminder", description="Set a reminder")
+async def reminder(interaction: discord.Interaction, duration_in_minutes: int, label: str):
+    await interaction.response.send_message(f"Reminder set for {duration_in_minutes} minutes for: {label}")
+    await asyncio.sleep(duration_in_minutes*60)
+    await interaction.followup.send(f"Reminder: {label}")
+
+#Starting the bot
+bot.run(TOKEN)
